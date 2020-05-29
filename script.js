b5765ddb48bfb6656bf8232c9c60d999248b7409
Lucas Antevere <lucas.antevere1901@gmail.com>
2020-05-28 23:59:02 -0300

    user card

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 
 $(function(){
-    // Check the initial Poistion of the Sticky Header
+
     var stickyHeaderTop = $('#bottomBar').offset().top;
 
     $(window).scroll(function(){
-            if( $(window).scrollTop() + 5 > stickyHeaderTop ) {
+
+            if($(window).scrollTop() + 1 > stickyHeaderTop ) {
                     $('#bottomBar').css({position: 'fixed', top: '0px'});
                     
             } else {
-                    $('#bottomBar').css({position: 'static', top: '0px'});
-                    
+                    $('#bottomBar').css({position: '', top: ' '});     
             }
     });
 });
